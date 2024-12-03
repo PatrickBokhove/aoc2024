cb803f701399ed70a4fbcd192adef0829ff0dbd7
Patrick Bokhove <patrickbokhove@gmail.com>
2024-12-03 19:24:40 +0100

    feat: day2

---
+++ day2.tsx
@@ -0,0 +1,58 @@
+import { day2 } from "./assignmentInputs";
+
+const inputAs2dNumberArray = day2
+  .split("\n")
+  .filter(Boolean)
+  .map((line) => line.split(" ").map((value) => parseInt(value)));
+
+// PART 1
+
+let amountOfSafeReports = 0;
+const minDeviation = 1;
+const maxDeviation = 3;
+
+function isLineOrderedAndWithinDeviationBounds(arr: number[]): boolean {
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] <= arr[i - 1]) {
+      return false; // Either duplicate or not sorted
+    }
+
+    const diff = Math.abs(arr[i] - arr[i - 1]);
+    if (diff < minDeviation || diff > maxDeviation) {
+      return false; // Not within deviation
+    }
+  }
+  return true; // The array is sorted and has no duplicates
+}
+
+// PART 2
+
+function isSafeWithOneRemoval(line: number[]): boolean {
+  for (let i = 0; i < line.length; i++) {
+    const modifiedLine = [...line.slice(0, i), ...line.slice(i + 1)];
+    if (isLineOrderedAndWithinDeviationBounds(modifiedLine)) {
+      return true; // Safe after removing one element
+    }
+  }
+  return false; // No removal made the line safe
+}
+
+inputAs2dNumberArray.forEach((line) => {
+  const valuesOkay = isLineOrderedAndWithinDeviationBounds(line);
+  const reversedValuesOkay = isLineOrderedAndWithinDeviationBounds(
+    line.reverse()
+  );
+  const safeWithOneRemoval = isSafeWithOneRemoval(line);
+  const reversedSafeWithOneRemoval = isSafeWithOneRemoval(line.reverse());
+
+  if (
+    valuesOkay ||
+    reversedValuesOkay ||
+    safeWithOneRemoval ||
+    reversedSafeWithOneRemoval
+  ) {
+    amountOfSafeReports++;
+  }
+});
+
+console.log("amount of initially safe reports", amountOfSafeReports);
