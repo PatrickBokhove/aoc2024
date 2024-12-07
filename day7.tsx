bcc4621d480ba2ae15b3498fc553c42ad3bc5c37
Patrick Bokhove <patrickbokhove@gmail.com>
2024-12-07 16:07:29 +0100

    feat: day 7 part 1

---
+++ day7.tsx
@@ -0,0 +1,49 @@
+// https://adventofcode.com/2024/day/7
+
+import { day7 } from "./assignmentInputs";
+
+// PART 1
+
+const playingField = day7.split("\n").filter(Boolean);
+const allowedOperators = ["+", "*"];
+
+let sumOfSolvedValues = 0;
+
+playingField.forEach((line) => {
+  let solved = false;
+  const testValue = parseInt(line.split(":")[0]);
+  const options = line
+    .split(": ")[1]
+    .split(" ")
+    .map((val) => parseInt(val));
+
+  function solveEquation(
+    options: number[],
+    currentEquation: string,
+    currentTotal: number
+  ) {
+    if (solved) {
+      return;
+    }
+
+    if (options.length === 0) {
+      if (currentTotal === testValue) {
+        sumOfSolvedValues += testValue;
+        solved = true;
+      }
+      return;
+    }
+
+    allowedOperators.forEach((operator) => {
+      solveEquation(
+        options.slice(1),
+        currentEquation + operator + options[0],
+        operator === "+" ? currentTotal + options[0] : currentTotal * options[0]
+      );
+    });
+  }
+
+  solveEquation(options.slice(1), options[0].toString(), options[0]);
+});
+
+console.log("sum of solved values", sumOfSolvedValues);
